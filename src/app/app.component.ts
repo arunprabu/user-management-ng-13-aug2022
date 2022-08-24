b7805667dc97d0c5a1dbccdb902cfc1129fbebf9
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-08-24 11:44:08 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+// Decorator 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // comp is exposed in a selector app-root 
+  templateUrl: './app.component.html', // html 
+  styleUrls: ['./app.component.css'] // css 
 })
 export class AppComponent {
+  // ts 
   title = 'user-management-ng-13';
 }
